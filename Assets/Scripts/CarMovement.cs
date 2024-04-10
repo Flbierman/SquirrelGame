db6a71bd1dd3dfea114c3fcb111201ee7e6f8dc0
KathrynMarieIrv <124322707+KathrynMarieIrv@users.noreply.github.com>
2024-04-10 15:30:59 -0700

    added trigger box collider on front and updated script

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -5,41 +5,23 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 public class CarMovement : MonoBehaviour
+
 {
-    public Vector2 direction = Vector2.right;
-
-    public float speed = 1f;
-
-    public int size = 1;
-
-    private Vector3 leftEdge;
-    private Vector3 rightEdge;
+    Rigidbody m_rigidbody;
+    float m_speed;
 
     private void Start()
 
     {
-     leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
-        rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+        m_rigidbody = GetComponent<Rigidbody>();
+        m_speed = 5f;
+
     }
 
     private void Update()
     {
-        if (direction.x > 0 && (transform.position.x - size) > rightEdge.x)
-        {
-            Vector3 position = transform.position;
-            position.x = leftEdge.x - size;
-            transform.position = position;
+        m_rigidbody.velocity = transform.forward * m_speed;
+
         }
-        else if (direction.x < 0 && (transform.position.x + size) > leftEdge.x)
-        {
-            Vector3 position = transform.position;
-            position.x = rightEdge.x + size;
-            transform.position = position;
-        }
-        else
-            {
-            transform.Translate(direction * speed * Time.deltaTime);
-        }
-    }
     }
 
