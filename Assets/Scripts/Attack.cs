c4ab26c47c22def40752a802f98512e37dd9f879
Peter Hartnett <peter.hartnett@gmail.com>
2024-04-16 20:58:55 -0400

    Added HP and Damage scripts, demo in SampleScene

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Attach this to an Attacking object and make a collider with isTrigger enabled on the attacker, large enough for the player to enter into.
+/// </summary>
+public class Attack : MonoBehaviour
+{
+    public int damage = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
